6470cb876d50ad6272898c6726fd4ff2a66c3e71
vodkyle <98049502+vodkyle@users.noreply.github.com>
2023-10-04 17:30:41 -0400

    Update student.py

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -1,1 +1,13 @@
+from App.models import Student
+from App.database import db
+from App.config import config 
+import requests 
+import json
 
+def add_student(studenID, name, degree): 
+
+def update_student(studentID): 
+
+def search_student(studentID): 
+
+def get_student(studentID): 
