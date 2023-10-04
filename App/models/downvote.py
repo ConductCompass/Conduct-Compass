f5b55d87b966ca856e8ada533e37586c78df9b56
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-10-04 16:08:06 -0400

    Update downvote.py

--- App/models/downvote.py
+++ App/models/downvote.py
@@ -5,3 +5,16 @@ class Downvote(db.Model):
     downvoteID = db.Column(db.Integer, primary_key = True)
     staffID = db.Column(db.Integer, db.ForeignKey('staff.staffID'))
     reviewID = db.Column(db.Integer, db.ForeignKey('review.reviewID'))
+
+    def __init__(self, downvoteID, staffID, reviewID):
+        self.downvoteID = downvoteID
+        self.staffID = staffID
+        self.reviewID = reviewID
+
+
+    def get_json(): 
+        return{
+            'downvoteID': downvoteID, 
+            'staffID': staffID, 
+            'reviewID': reviewID
+        }
