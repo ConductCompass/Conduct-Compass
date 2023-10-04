4323cd7aa262339912db5fae06e127d2956b7ba7
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-10-04 16:00:03 -0400

    Update staff.py

--- App/models/staff.py
+++ App/models/staff.py
@@ -4,3 +4,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class Staff(db.Model):
     staffID = db.Column(db.Integer, primary_key = True)
     reviews_logged = db.Column(db.String(120))
+    staffName = db.Column(db.String(120))
+
+    def __init__(self, staffID, reviews_logged): 
+        self.staffID = staffID
+        self.reviews_logged = reviews_logged
+    
+    def get_json(self): 
+        return{
+            'staffID': staffID,
+            'staffName': staffName, 
+            'reviews_logged': reviews_logged
+        }
