5236124ab77920ba193daa0fda0055bd0ecab409
karissa-ss <98049869+karissa-ss@users.noreply.github.com>
2023-10-04 16:26:58 -0400

    Update upvote.py

--- App/models/upvote.py
+++ App/models/upvote.py
@@ -5,3 +5,15 @@ class Upvote(db.Model):
     upvoteID = db.Column(db.Integer, primary_key = True)
     staffID = db.Column(db.Integer, db.ForeignKey('staff.staffID'))
     reviewID = db.Column(db.Integer, db.ForeignKey('review.reviewID'))
+
+    def __init__(self, upvoteID, staffID, reviewID): 
+        self.upvote = upvote
+        self.staffID = staffID
+        self.reviewID = reviewID
+
+    def get_json(self): 
+        return{
+            'upvote': upvote,
+            'staffID': staffID, 
+            'reviewID': reviewID
+        }
