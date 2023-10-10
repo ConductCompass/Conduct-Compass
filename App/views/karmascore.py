7b4f15caaa2c4b1c6c3ce36db84af1fb47c7fb07
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2023-10-10 18:52:51 -0400

    Update karmascore.py

--- App/views/karmascore.py
+++ App/views/karmascore.py
@@ -2,8 +2,9 @@ from flask import Blueprint, render_template, jsonify, request, send_from_direct
 
 from App.controllers import (calculate_karma_score)
 
+karmascore_views = Blueprint('karmascore_views', __name__, template_folder='../templates')
+
 @karmascore_views.route('/karmascore', methods = ['GET'])
 def karma_page():
-    score = calculate_karma_score()
-    return score
-    
+    #score = calculate_karma_score()
+    return render_template('index.html')
