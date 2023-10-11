b0def6061d437f4cb5165bd63288203602d3254b
Justin Jaikaran <127766936+JustinJ132@users.noreply.github.com>
2023-10-10 20:56:26 -0400

    Update __init__.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,5 +4,7 @@ from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .karmascore import karmascore_views
+from .students import student_views
 
-views = [user_views, index_views, auth_views, karmascore_views] 
+
+views = [user_views, index_views, auth_views, student_views, karmascore_views] 
